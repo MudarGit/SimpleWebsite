2392b35cc6a7130d69ece7c90bc32d6d152e928b
MudarGit <kneehighmud@gmail.com>
2015-04-25 14:33:23 -0400

    + location in hero box

--- SimpleWebsite/Controllers/HomeController.cs
+++ SimpleWebsite/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;  
 
 namespace SimpleWebsite.Controllers
 {
@@ -10,6 +11,8 @@ namespace SimpleWebsite.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Location = ConfigurationManager.AppSettings["location"];
+            
             return View();
         }
 
